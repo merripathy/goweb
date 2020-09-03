e1807802f4a2a892a3cce6b99ed4811b82566309
Merri <merri@merri.com>
2020-09-03 12:46:44 +0530

    init changed

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ type user struct {
 func init() {
 
 	dbUsers["bmerri"] = user{"bmerri", "pass", "bmerri@abc.com", 110.00}
-	fmt.Println("Program started and init function called")
+	fmt.Println("Program started and init function is called")
 
 }
 
